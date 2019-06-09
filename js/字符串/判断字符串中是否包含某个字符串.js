d150f029dbb3f085a0cee02ad0f466f292ebea3e
doveyyang <tnbird@163.com>
2019-06-09 11:43:15 +0800

    update

---
+++ js/字符串/判断字符串中是否包含某个字符串.js
@@ -0,0 +1,12 @@
+// 判断字符串中是否包含某个字符串
+let str = 'pages/index/index'
+let ostr = '/pages/index/index'
+
+if(ostr.indexOf(str)!=-1){
+    console.log('exist')
+}else{
+    console.log('no exist')
+}
+
+// 
+
